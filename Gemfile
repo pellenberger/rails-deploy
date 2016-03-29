8d2034a44f66fa96b84eb47ac90b25d035c3f4b2
pellenberger <p.ellenberger@outlook.com>
2016-03-29 09:51:32 +0200

    Configure deployment

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ group :development do
   gem 'spring'
 end
 
-gem 'capistrano', '~> 3.1.0'
+gem 'capistrano', '~> 3.4.0'
 gem 'capistrano-bundler', '~> 1.1.2'
 gem 'capistrano-rails', '~> 1.1.1'
 gem 'capistrano-rvm', github: "capistrano/rvm"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{192.168.74.128}
+role :web, %w{192.168.74.128}
+role :db,  %w{192.168.74.128}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +14,8 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :stage, :production
+server '192.168.74.128', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
